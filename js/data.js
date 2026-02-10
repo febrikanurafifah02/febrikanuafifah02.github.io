11eb7dc9eb98f18e6072998660ecb5f2b0889204
febrikanurafifah02 <febrikanurafifah736@gmail.com>
2026-02-10 17:29:02 +0700

    Update data.js
    Signed-off-by: febrikanurafifah02 <febrikanurafifah736@gmail.com>

--- js/data.js
+++ js/data.js
@@ -1,1 +1,6 @@
-
+const books=[
+{title:"Artificial Intelligence Library",author:"B. Putra"},
+{title:"Manajemen Informasi",author:"A. Siregar"},
+{title:"Digital Literacy",author:"C. Rahma"},
+{title:"Knowledge Management",author:"D. Pratama"}
+];
