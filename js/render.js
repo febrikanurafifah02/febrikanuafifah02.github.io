5c649283733d6f838dac1cc76c8d65497bbe8457
febrikanurafifah02 <febrikanurafifah736@gmail.com>
2026-02-10 17:29:54 +0700

    Update render.js
    Signed-off-by: febrikanurafifah02 <febrikanurafifah736@gmail.com>

--- js/render.js
+++ js/render.js
@@ -1,1 +1,14 @@
-
+function renderBooks(){
+const bookList=document.getElementById('bookList');
+if(!bookList) return;
+bookList.innerHTML="";
+books.forEach(b=>{
+bookList.innerHTML+=`
+<div class="col-md-3">
+<div class="card card-book p-3 mb-3">
+<h6>${b.title}</h6>
+<small>${b.author}</small>
+</div>
+</div>`;
+});
+}
